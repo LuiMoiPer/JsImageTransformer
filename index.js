f40a5df3c9bbe06588524e7de38c4b7bf52ad233
LuiMoiPer <luimoiper@outlook.com>
2022-06-22 16:56:18 -0700

    Made the transfromation only take place after the user image loads

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-import ImageTransformer from "./src/ImageTransformer.js";
-
 const imageInput = document.querySelector("#image_input");
+const worker = new Worker("worker.js");
 
 imageInput.addEventListener("change", function(){
     const fileReader = new FileReader();
     fileReader.addEventListener("load", () => {
         displayImage(fileReader.result);
-        makeNewImage();
+        makeNewImage(fileReader.result);
     });
     fileReader.readAsDataURL(this.files[0]);
 }); 
 
 function displayImage(result) {
     // set the preview to show the image they selected
-    const userImage = document.querySelector("#user_image");
+    let userImage = document.querySelector("#user_image");
     userImage.src = result;
-
-    // Make an image to store the data of the file selected
-    let image = new Image();
-    image.src = result;
-
-    // Make the canvas match the size of the image and draw it to the canvas
-    const canvas = document.querySelector("#output_canvas");
-    const context = canvas.getContext("2d");
-    context.canvas.width = image.width;
-    context.canvas.height = image.height;
-    context.drawImage(image, 0, 0);
 }
 
-function makeNewImage() {
-    // Grab the canvas and extract the byte data
-    const canvas = document.querySelector("#output_canvas")
-    const context = canvas.getContext("2d");
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    const transfromer = new ImageTransformer(imageData);
-    transfromer.transfrom();
-    let transformedData = transfromer.getImageData();
-    context.putImageData(transformedData, 0, 0);
+function makeNewImage(result) {
+    let image = new Image();
+    image.onload = function () {
+        // Make the canvas match the size of the image and draw it to the canvas
+        let canvas = document.querySelector("#output_canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        let context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0);
+
+        let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        worker.postMessage(imageData);
+        worker.onmessage = function(message) {
+            context.putImageData(message.data, 0, 0);
+        }
+    }
+    image.src = result;
 }

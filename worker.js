f058ef997c17fa5c4d19d0eca375860eac58ba28
LuiMoiPer <luimoiper@outlook.com>
2022-06-23 22:45:27 -0700

    Added a log message when the image transformation is finished.

--- worker.js
+++ worker.js
@@ -4,5 +4,6 @@ self.onmessage = function(message) {
     console.log("Message from web worker:" + message.data);
     let transformer = new ImageTransformer(message.data);
     transformer.transform();
+    console.log("Finished image transformation, sending data to main thread");
     self.postMessage(transformer.getImageData());
 }; 
